c6f7255df41df20f16beb4a171d3d7a6ab896bee
xSunnyyy <sunny.ownz@gmail.com>
2025-07-19 18:01:52 -0400

    Create TradeTransaction.svelte

---
+++ src/lib/components/TradeTransaction.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let transaction;
+  export let players;
+  export let leagueTeamManagers;
+</script>
+
+<div>
+  Trade Transaction between: {transaction.rosters.join(' & ')}
+</div>
