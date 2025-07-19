c195555a2389168db52ecf85a795fa967bfb0830
xSunnyyy <sunny.ownz@gmail.com>
2025-07-19 19:25:56 -0400

    Update TradeTransaction.svelte

--- src/lib/components/TradeTransaction.svelte
+++ src/lib/components/TradeTransaction.svelte
@@ -7,117 +7,116 @@
 </script>
 
 <style>
-    .tradeTransaction {
-        display: flex;
-        position: relative;
-        flex-direction: column;
-        margin-bottom: 1em;
-    }
-    
-    .name {
-        position: relative;
-        text-align: center;
-    }
+	.tradeTransaction {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 300px;
+		margin: 0.8rem auto;
+		padding: 0.9rem;
+		border-radius: 1rem;
+		background: #f8f8f8;
+		border: 1px solid #ddd;
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
+		color: #000;
+		font-size: 0.85rem;
+	}
 
-    .avatar {
-        border-radius: 50%;
-        height: 40px;
-        width: 40px;
-        border: 2px solid var(--blueOne);
-        background-color: var(--fff);
-    }
+	.name {
+		text-align: center;
+		margin-bottom: 0.6rem;
+	}
 
-    .ownerName {
-        display: inline-block;
-        font-weight: normal;
-        line-height: 1em;
-        margin: 0.2em;
-    }
+	.avatar {
+		border-radius: 50%;
+		height: 36px;
+		width: 36px;
+		border: 2px solid var(--blueTwo);
+		background-color: #fff;
+	}
 
-    .currentOwner {
-        font-style: italic;
-        color: var(--aaa);
-        font-size: 0.7em;
-    }
+	.ownerName {
+		display: inline-block;
+		font-weight: 600;
+		font-size: 0.9rem;
+		border-bottom: 2px solid var(--blueTwo);
+	}
 
-    .clickable {
-        cursor: pointer;
-    }
+	.currentOwner {
+		font-style: italic;
+		color: #888;
+		font-size: 0.65rem;
+		margin-left: 4px;
+	}
 
-    .date {
-        color: var(--g999);
-        font-style: italic;
-        font-size: 0.7em;
-        text-align: center;
-        padding: 0.7em 0 1em;
-        background-color: var(--fff);
-        border-radius: 0 0 0 40px;
-        border-left: 2px solid var(--blueOne);
-        border-right: 1px solid var(--ddd);
-        margin-bottom: 3em;
-    }
+	table {
+		width: 100%;
+		border-collapse: collapse;
+	}
 
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        table-layout: fixed;
-        /*
-            the height setting is ignored, but
-            allows the holder class div to have
-            a height of 100%
-        */
-        height: 1px;
-    }
+	thead {
+		margin-bottom: 0.5rem;
+	}
 
-    tbody {
-        background-color: var(--fff);
-        border-top: 2px solid var(--blueOne);
-        border-left: 2px solid var(--blueOne);
-        border-right: 1px solid var(--ddd);
-    }
+	tbody {
+		border-top: 1px solid #eee;
+	}
 
-    .holder {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        height: 100%;
-    }
+	.holder {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 
-    @media (max-width: 420px) {
-        .ownerName {
-            font-size: 0.8em;
-        }
-    }
+	.date {
+		text-align: center;
+		font-size: 0.65rem;
+		color: #888;
+		font-style: italic;
+		margin-top: 0.5rem;
+	}
+
+	.clickable {
+		cursor: pointer;
+	}
+
+	@media (max-width: 420px) {
+		.tradeTransaction {
+			max-width: 92vw;
+			padding: 0.7rem;
+		}
+
+		.ownerName {
+			font-size: 0.8rem;
+		}
+	}
 </style>
 
 <div class="tradeTransaction">
-    <table>
-        <thead>
-            <tr>
-                {#each transaction.rosters as owner}
-                    <th class="name clickable" style="width: {1 / transaction.rosters.length * 100}%;" onclick={() => gotoManager({year: transaction.season, leagueTeamManagers, rosterID: owner})}>
-                        <div class="holder">
-                            <img class="avatar" src="{getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).avatar}" alt="{getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).name} avatar"/>
-                            <span class="ownerName">
-                                {getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).name}
-                                {#if getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).name != getTeamFromTeamManagers(leagueTeamManagers, owner).name}
-                                    <br />
-                                    <span class="currentOwner">({getTeamFromTeamManagers(leagueTeamManagers, owner).name})</span>
-                                {/if}
-                            </span>
-                        </div>
-                    </th>
-                {/each}
-            </tr>
-        </thead>
-        <tbody>
-            {#each transaction.moves as move}
-                <TransactionMove {players} {move} type={transaction.type} {leagueTeamManagers} season={transaction.season} />
-            {/each}
-        </tbody>
-    </table>
-    <span class="date">
-        {transaction.date}
-    </span>
+	<table>
+		<thead>
+			<tr>
+				{#each transaction.rosters as owner}
+					<th class="name clickable" onclick={() => gotoManager({ year: transaction.season, leagueTeamManagers, rosterID: owner })}>
+						<div class="holder">
+							<img class="avatar" src="{getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).avatar}" alt="{getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).name} avatar" />
+							<span class="ownerName">
+								{getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).name}
+								{#if getTeamFromTeamManagers(leagueTeamManagers, owner, transaction.season).name !== getTeamFromTeamManagers(leagueTeamManagers, owner).name}
+									<br />
+									<span class="currentOwner">({getTeamFromTeamManagers(leagueTeamManagers, owner).name})</span>
+								{/if}
+							</span>
+						</div>
+					</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each transaction.moves as move}
+				<TransactionMove {players} {move} type={transaction.type} {leagueTeamManagers} season={transaction.season} />
+			{/each}
+		</tbody>
+	</table>
+	<span class="date">{transaction.date}</span>
 </div>
