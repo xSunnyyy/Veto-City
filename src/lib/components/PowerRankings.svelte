c860ae7cdba78ccf12c48fa499ff62693bba8208
xSunnyyy <sunny.ownz@gmail.com>
2025-07-19 19:50:47 -0400

    Update PowerRankings.svelte

--- src/lib/components/PowerRankings.svelte
+++ src/lib/components/PowerRankings.svelte
@@ -3,61 +3,32 @@
   import PowerRankingsDisplay from './PowerRankingsDisplay.svelte';
   import LinearProgress from '@smui/linear-progress';
 
-  let loading = true;
-  let nflState, rostersData, leagueTeamManagers, leagueData, playersInfo;
-  let error = null;
-
-  onMount(async () => {
-    try {
-      [nflState, rostersData, leagueTeamManagers, leagueData, playersInfo] = await waitForAll(
-        getNflState(),
-        getLeagueRosters(),
-        getLeagueTeamManagers(),
-        getLeagueData(),
-        loadPlayers(null)
-      );
-    } catch (err) {
-      error = err;
-    } finally {
-      loading = false;
-    }
-  });
+  const helperPromises = waitForAll(
+    getNflState(),
+    getLeagueRosters(),
+    getLeagueTeamManagers(),
+    getLeagueData(),
+    loadPlayers(null)
+  );
 </script>
 
-<style>
-  .loading {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-    text-align: center;
-    color: var(--text-color, #333);
-  }
-
-  .error {
-    color: red;
-    padding: 1rem;
-    text-align: center;
-  }
-</style>
-
-{#if loading}
-  <div class="loading">
+{#await helperPromises}
+  <div>
     <p>Calculating power rankings...</p>
     <LinearProgress indeterminate />
   </div>
-{:else if error}
-  <div class="error">
-    <p>Something went wrong: {error.message}</p>
-  </div>
-{:else if leagueData.status !== 'pre_draft' && leagueData.status !== 'complete'}
-  <PowerRankingsDisplay
-    {nflState}
-    {rostersData}
-    {leagueTeamManagers}
-    {leagueData}
-    {playersInfo}
-  />
-{:else}
-  <p style="text-align:center;">Power rankings not available for this league state.</p>
-{/if}
+{:then [nflState, rostersData, leagueTeamManagers, leagueData, playersInfo]}
+  {#if leagueData.status !== 'pre_draft' && leagueData.status !== 'complete'}
+    <PowerRankingsDisplay
+      {nflState}
+      {rostersData}
+      {leagueTeamManagers}
+      {leagueData}
+      {playersInfo}
+    />
+  {:else}
+    <p>Power rankings will be available during the season.</p>
+  {/if}
+{:catch error}
+  <p>Error loading power rankings: {error.message}</p>
+{/await}
