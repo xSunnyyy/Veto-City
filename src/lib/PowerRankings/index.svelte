7ebf4f35afc4a918358b1976e8e1d6c0b69054c2
xSunnyyy <sunny.ownz@gmail.com>
2025-07-19 17:21:06 -0400

    Update index.svelte

--- src/lib/PowerRankings/index.svelte
+++ src/lib/PowerRankings/index.svelte
@@ -1,8 +1,12 @@
 <script>
-    import {getNflState, getLeagueRosters, getLeagueTeamManagers, waitForAll, loadPlayers, getLeagueData} from '$lib/utils/helper';
+    // Carousel props for slicing
+    export let start = 0;
+    export let count = null;
+
+    import { getNflState, getLeagueRosters, getLeagueTeamManagers, waitForAll, loadPlayers, getLeagueData } from '$lib/utils/helper';
     import PowerRankingsDisplay from './PowerRankingsDisplay.svelte';
     import LinearProgress from '@smui/linear-progress';
-    
+
     const helperPromises = waitForAll(
         getNflState(),
         getLeagueRosters(),
@@ -10,7 +14,6 @@
         getLeagueData(),
         loadPlayers(null),
     );
-
 </script>
 
 <style>
@@ -23,19 +26,30 @@
 </style>
 
 {#await helperPromises}
-    <!-- promise is pending -->
     <div class="loading">
         <p>Calculating power rankings...</p>
         <LinearProgress indeterminate />
     </div>
 {:then [nflState, rostersData, leagueTeamManagers, leagueData, playersInfo]}
     {#if leagueData.status != 'pre_draft' && leagueData.status != 'complete'}
-        <PowerRankingsDisplay {nflState} {rostersData} {leagueTeamManagers} {leagueData} {playersInfo} />
+        {#if count === null}
+            <PowerRankingsDisplay
+                {nflState}
+                {rostersData}
+                {leagueTeamManagers}
+                {leagueData}
+                {playersInfo}
+            />
+        {:else}
+            <PowerRankingsDisplay
+                {nflState}
+                rostersData={rostersData.slice(start, start + count)}
+                {leagueTeamManagers}
+                {leagueData}
+                {playersInfo}
+            />
+        {/if}
     {/if}
 {:catch error}
-	<!-- promise was rejected -->
-	<p>Something went wrong: {error.message}</p>
+    <p>Something went wrong: {error.message}</p>
 {/await}
-
-
-
