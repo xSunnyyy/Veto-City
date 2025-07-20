ba7767c545c1463ea89c3ee36b4f48d73bf00fcd
xSunnyyy <sunny.ownz@gmail.com>
2025-07-20 13:10:40 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,16 @@
+<!-- __layout.svelte -->
 <script>
-	import { Nav, Footer } from "$lib/components";
-	import { dev } from '$app/environment';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	import { Nav, Footer } from "$lib/components"
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
-<style>
-	:global(body) {
-		margin: 0;
-		padding: 0;
-		background-color: #111; /* global black background */
-		font-family: system-ui, sans-serif;
-	}
-
-	:global(h1),
-	:global(h3),
-	:global(h5),
-	:global(input),
-	:global(span) {
-		color: white;
-	}
-
-	.page-wrapper {
-		background-color: #111;
-		min-height: 100vh;
-		padding: 2rem 1rem;
-	}
-</style>
-
 <main>
-	<Nav />
+    <Nav /> <!-- adds the nav (small and large) -->
+  
+    <slot />
 
-	<div class="page-wrapper">
-		<slot />
-	</div>
-
-	<Footer />
+    <Footer /> <!-- adds the footer -->
 </main>
-
